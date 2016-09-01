b300704f406ed41dd7d30b76eebb8abc7e4b04ea
Craig Weber <weberc2@gmail.com>
2016-09-01 12:28:55 -0500

    Init

---
+++ csv.go
@@ -0,0 +1,203 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"unicode/utf8"
+)
+
+type bufferedReader struct {
+	r      io.Reader
+	data   []byte
+	cursor int
+}
+
+func (b *bufferedReader) peekRune() (rune, int, error) {
+START:
+	r, size := utf8.DecodeRune(b.data[b.cursor:])
+	if r == utf8.RuneError {
+		if size == 0 {
+			if err := b.more(); err != nil {
+				return r, size, err
+			}
+			goto START
+		}
+		if size == 1 {
+			return r, size, fmt.Errorf("Invalid UTF-8")
+		}
+	}
+	return r, size, nil
+}
+
+func (b *bufferedReader) nextRune() (rune, int, error) {
+	r, size, err := b.peekRune()
+	b.cursor += size
+	return r, size, err
+}
+
+var counter = 0
+
+func (b *bufferedReader) more() error {
+	if len(b.data) == cap(b.data) {
+		counter++
+		println("Hit", counter, "times; prev size:", len(b.data))
+		println("Data:", string(b.data[0:200]))
+		temp := make([]byte, len(b.data), 2*len(b.data)+1)
+		copy(temp, b.data)
+		b.data = temp
+	}
+
+	// read the new bytes onto the end of the buffer
+	n, err := b.r.Read(b.data[len(b.data):cap(b.data)])
+	b.data = b.data[:len(b.data)+n]
+	return err
+}
+
+func (b *bufferedReader) reset() {
+	copy(b.data, b.data[b.cursor:])
+	b.data = b.data[:len(b.data)-b.cursor]
+	println("RESET")
+	b.cursor = 0
+}
+
+func (b *bufferedReader) slice(start, end int) []byte {
+	return b.data[start:end]
+}
+
+func (b *bufferedReader) sliceFrom(start int) []byte {
+	return b.data[start:b.cursor]
+}
+
+type fields struct {
+	fieldStart int
+	buffer     bufferedReader
+	hitEOL     bool
+	field      []byte
+	err        error
+}
+
+func (fs *fields) Reset() {
+	fs.buffer.reset()
+	fs.field = nil
+	fs.fieldStart = 0
+	fs.hitEOL = false
+}
+
+func (fs *fields) nextUnquotedField() bool {
+	const sizeEOL = 1
+	const sizeDelim = 1
+	for {
+		ch, _, err := fs.buffer.nextRune()
+		if err != nil {
+			if err == io.EOF {
+				fs.field = fs.buffer.data[fs.fieldStart:fs.buffer.cursor]
+				fs.hitEOL = true
+				fs.err = err
+				return true
+			}
+			fs.err = err
+			return false
+		}
+
+		switch ch {
+		case ',':
+			fs.field = fs.buffer.data[fs.fieldStart : fs.buffer.cursor-sizeDelim]
+			fs.fieldStart = fs.buffer.cursor
+			return true
+		case '\n':
+			fs.field = fs.buffer.data[fs.fieldStart : fs.buffer.cursor-sizeEOL]
+			fs.hitEOL = true
+			return true
+		default:
+			continue
+		}
+	}
+}
+
+func nextQuotedField(buffer *bufferedReader) ([]byte, bool, error) {
+	// skip past the initial quote rune; we already checked the error when we
+	// peeked it before this method call, so no need to handle it again
+	buffer.nextRune()
+	start := buffer.cursor
+
+	writeCursor := buffer.cursor
+	last := rune(0)
+	for {
+		r, size, err := buffer.nextRune()
+		if err != nil {
+			return buffer.data[start:writeCursor], true, err
+		}
+		switch r {
+		case ',':
+			if last == '"' {
+				return buffer.data[start:writeCursor], false, nil
+			}
+		case '\n':
+			if last == '"' {
+				return buffer.data[start:writeCursor], true, nil
+			}
+		case '"':
+			if last != '"' { // only reduce the size for the first '"'
+				last = r
+				continue
+			}
+		}
+		writeCursor += size
+		last = r
+		// copy the current rune onto writeCursor if writeCursor !=
+		// buffer.cursor
+		if writeCursor != buffer.cursor {
+			copy(
+				buffer.data[writeCursor:writeCursor+size],
+				buffer.data[buffer.cursor:buffer.cursor+size],
+			)
+		}
+	}
+}
+
+func (fs *fields) next() bool {
+	if fs.hitEOL {
+		return false
+	}
+	first, _, err := fs.buffer.peekRune()
+	if err != nil {
+		fs.err = err
+		return false
+	}
+
+	if first == '"' {
+		fs.field, fs.hitEOL, fs.err = nextQuotedField(&fs.buffer)
+		return fs.err == nil || fs.err == io.EOF
+	}
+	return fs.nextUnquotedField()
+}
+
+type CSVReader struct {
+	fields       fields
+	fieldsBuffer [][]byte
+}
+
+func (r *CSVReader) Read() ([][]byte, error) {
+	if err := r.fields.err; err != nil {
+		return nil, err
+	}
+	r.fields.Reset()
+	r.fieldsBuffer = r.fieldsBuffer[:0]
+	for r.fields.next() {
+		r.fieldsBuffer = append(r.fieldsBuffer, r.fields.field)
+	}
+	if err := r.fields.err; err != nil && err != io.EOF {
+		return nil, err
+	}
+	return r.fieldsBuffer, nil
+}
+
+func NewCSVReader(r io.Reader) CSVReader {
+	return CSVReader{
+		fields: fields{
+			buffer: bufferedReader{r: r, data: make([]byte, 0, 242)},
+		},
+		fieldsBuffer: make([][]byte, 0, 242),
+	}
+}
---
+++ csv_test.go
@@ -0,0 +1,241 @@
+package main
+
+import (
+	"bytes"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"strings"
+	"testing"
+)
+
+func toStrings(bs [][]byte) []string {
+	strs := make([]string, 0, len(bs))
+	for _, b := range bs {
+		strs = append(strs, string(b))
+	}
+	return strs
+}
+
+func compareLine(line [][]byte, wanted ...string) error {
+	if len(line) != len(wanted) {
+		return fmt.Errorf(
+			"Wanted [%s]; got [%s]",
+			strings.Join(wanted, ", "),
+			strings.Join(toStrings(line), ", "),
+		)
+	}
+	for i, s := range toStrings(line) {
+		if s != wanted[i] {
+			return fmt.Errorf(
+				"Mismatch at item %d; wanted '%s'; got '%s'",
+				i,
+				wanted[i],
+				s,
+			)
+		}
+	}
+	return nil
+}
+
+func TestReadOneRow(t *testing.T) {
+	r := NewCSVReader(strings.NewReader("abc,def,ghi"))
+
+	fields, err := r.Read()
+	if err != nil {
+		t.Fatal("Unexpected error:", err)
+	}
+
+	if err := compareLine(fields, "abc", "def", "ghi"); err != nil {
+		t.Fatal(err)
+	}
+
+	if _, err = r.Read(); err != io.EOF {
+		t.Fatal("Wanted io.EOF; got:", err)
+	}
+}
+
+func TestReadMultipleLines(t *testing.T) {
+	r := NewCSVReader(strings.NewReader("abc,def\n1234,56"))
+
+	fields, err := r.Read()
+	if err != nil {
+		t.Fatal("Unexpected error:", err)
+	}
+
+	if err := compareLine(fields, "abc", "def"); err != nil {
+		t.Fatal(err)
+	}
+
+	fields, err = r.Read()
+	if err != nil {
+		t.Fatal("Unexpected error:", err)
+	}
+	if err := compareLine(fields, "1234", "56"); err != nil {
+		t.Fatal(err)
+	}
+
+	if _, err := r.Read(); err != io.EOF {
+		t.Fatal("Wanted io.EOF; got:", err)
+	}
+}
+
+func TestReadQuotedField(t *testing.T) {
+	r := NewCSVReader(strings.NewReader("\"abc\",\"123\",\"456\""))
+
+	fields, err := r.Read()
+	if err != nil {
+		t.Fatal("Unexpected error:", err)
+	}
+	if err := compareLine(fields, "abc", "123", "456"); err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = r.Read()
+	if err != io.EOF {
+		t.Fatal("Wanted io.EOF; got:", err)
+	}
+}
+
+func TestReadQuotedFieldMultipleLines(t *testing.T) {
+	r := NewCSVReader(strings.NewReader("\"abc\",\"123\"\n\"def\",\"456\""))
+
+	fields, err := r.Read()
+	if err != nil {
+		t.Fatal("Unexpected error:", err)
+	}
+	if err := compareLine(fields, "abc", "123"); err != nil {
+		t.Fatal(err)
+	}
+
+	fields, err = r.Read()
+	if err != nil {
+		t.Fatal("Unexpected error:", err)
+	}
+	if err := compareLine(fields, "def", "456"); err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = r.Read()
+	if err != io.EOF {
+		t.Fatal("Wanted io.EOF; got:", err)
+	}
+}
+
+func TestReadQuotedFieldsWithComma(t *testing.T) {
+	r := NewCSVReader(strings.NewReader("\"a,b,c\",\"d,e,f\""))
+
+	fields, err := r.Read()
+	if err != nil {
+		t.Fatal("Unexpected error:", err)
+	}
+	if err := compareLine(fields, "a,b,c", "d,e,f"); err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = r.Read()
+	if err != io.EOF {
+		t.Fatal("Wanted io.EOF; got:", err)
+	}
+}
+
+func TestReadQuotedFieldsWithNewLine(t *testing.T) {
+	r := NewCSVReader(strings.NewReader("\"a\nb\nc\""))
+
+	fields, err := r.Read()
+	if err != nil {
+		t.Fatal("Unexpected error:", err)
+	}
+
+	if err := compareLine(fields, "a\nb\nc"); err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = r.Read()
+	if err != io.EOF {
+		t.Fatal("Wanted io.EOF; got:", err)
+	}
+}
+
+func TestReadQuotedFieldsWithEscapedQuotes(t *testing.T) {
+	r := NewCSVReader(strings.NewReader("\"a\"\"b\""))
+
+	fields, err := r.Read()
+	if err != nil {
+		t.Fatal("Unexpected error:", err)
+	}
+
+	if err := compareLine(fields, "a\"b"); err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = r.Read()
+	if err != io.EOF {
+		t.Fatal("Wanted io.EOF; got:", err)
+	}
+}
+
+func BenchmarkStdCsv(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		r := csv.NewReader(bytes.NewReader(_fl_insuranceCsv))
+		for {
+			if _, err := r.Read(); err != nil {
+				if err == io.EOF {
+					break
+				}
+				b.Fatal(err)
+			}
+		}
+	}
+}
+
+func BenchmarkMyCsv(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		r := NewCSVReader(bytes.NewReader(_fl_insuranceCsv))
+		for {
+			if _, err := r.Read(); err != nil {
+				if err == io.EOF {
+					break
+				}
+				b.Fatal(err)
+			}
+		}
+	}
+}
+
+func BenchmarkStdCsvQuoted(b *testing.B) {
+	data, err := ioutil.ReadFile("fl_insurance_quoted.csv")
+	if err != nil {
+		b.Fatal(err)
+	}
+	for i := 0; i < b.N; i++ {
+		r := csv.NewReader(bytes.NewReader(data))
+		for {
+			if _, err := r.Read(); err != nil {
+				if err == io.EOF {
+					break
+				}
+				b.Fatal(err)
+			}
+		}
+	}
+}
+
+func BenchmarkMyCsvQuoted(b *testing.B) {
+	data, err := ioutil.ReadFile("fl_insurance_quoted.csv")
+	if err != nil {
+		b.Fatal(err)
+	}
+	for i := 0; i < b.N; i++ {
+		r := NewCSVReader(bytes.NewReader(data))
+		for {
+			if _, err := r.Read(); err != nil {
+				if err == io.EOF {
+					break
+				}
+				b.Fatal(err)
+			}
+		}
+	}
+}
