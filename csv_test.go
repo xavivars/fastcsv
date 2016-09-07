0edaaba4129ed2469cc50c0b5718e638df3f93ae
Craig Weber <weberc2@gmail.com>
2016-09-07 17:40:02 -0500

    Created subtests for table-driven testing and subbenchmarks for shared setup

--- csv_test.go
+++ csv_test.go
@@ -10,20 +10,6 @@ import (
 	"testing"
 )
 
-var data, quotedData []byte
-
-func init() {
-	var err error
-	data, err = ioutil.ReadFile("testdata/fl_insurance.csv")
-	if err != nil {
-		panic(err)
-	}
-	quotedData, err = ioutil.ReadFile("testdata/fl_insurance_quoted.csv")
-	if err != nil {
-		panic(err)
-	}
-}
-
 func toStrings(bs [][]byte) []string {
 	strs := make([]string, 0, len(bs))
 	for _, b := range bs {
@@ -61,144 +47,143 @@ func compareLine(line [][]byte, wanted ...string) error {
 	return nil
 }
 
-func testCSV(source string, wanted [][]string) error {
-	r := NewReader(strings.NewReader(source))
-
-	for i, wantedLine := range wanted {
-		fields, err := r.Read()
-		if err != nil {
-			return fmt.Errorf("Unexpected error: %v", err)
-		}
-		if err := compareLine(fields, wantedLine...); err != nil {
-			return fmt.Errorf("Mismatch on line %d: %v", i+1, err)
-		}
+func TestRead(t *testing.T) {
+	testCases := []struct {
+		Title  string
+		Input  string
+		Wanted [][]string
+	}{
+		{
+			Title:  "OneRow",
+			Input:  "abc,def,ghi",
+			Wanted: [][]string{{"abc", "def", "ghi"}},
+		},
+		{
+			Title:  "MultipleLines",
+			Input:  "abc,def\n1234,56",
+			Wanted: [][]string{{"abc", "def"}, {"1234", "56"}},
+		},
+		{
+			Title:  "QuotedField",
+			Input:  "\"abc\",\"123\",\"456\"",
+			Wanted: [][]string{{"abc", "123", "456"}},
+		},
+		{
+			Title:  "QuotedFieldMultipleLines",
+			Input:  "\"abc\",\"123\"\n\"def\",\"456\"",
+			Wanted: [][]string{{"abc", "123"}, {"def", "456"}},
+		},
+		{
+			Title:  "QuotedFieldsWithComma",
+			Input:  "\"a,b,c\",\"d,e,f\"",
+			Wanted: [][]string{{"a,b,c", "d,e,f"}},
+		},
+		{
+			Title:  "QuotedFieldsWithNewLine",
+			Input:  "\"a\nb\nc\"",
+			Wanted: [][]string{{"a\nb\nc"}},
+		},
+		{
+			Title:  "QuotedFieldsWithEscapedQuotes",
+			Input:  "\"a\"\"b\"",
+			Wanted: [][]string{{"a\"b"}},
+		},
+		{
+			Title:  "TrailingNewline",
+			Input:  "a,b,c\n",
+			Wanted: [][]string{{"a", "b", "c"}},
+		},
+		{
+			Title:  "EmptyMiddleLine",
+			Input:  "a,b\n\nc,d",
+			Wanted: [][]string{{"a", "b"}, {""}, {"c", "d"}},
+		},
+		{
+			Title:  "CRLF",
+			Input:  "a,b,c\r\nd,e,f",
+			Wanted: [][]string{{"a", "b", "c"}, {"d", "e", "f"}},
+		},
 	}
 
-	if _, err := r.Read(); err != io.EOF {
-		return fmt.Errorf("Wanted io.EOF; got: %v", err)
-	}
-	return nil
-}
-
-func TestReadOneRow(t *testing.T) {
-	wanted := [][]string{{"abc", "def", "ghi"}}
-	if err := testCSV("abc,def,ghi", wanted); err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestReadMultipleLines(t *testing.T) {
-	wanted := [][]string{{"abc", "def"}, {"1234", "56"}}
-	if err := testCSV("abc,def\n1234,56", wanted); err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestReadQuotedField(t *testing.T) {
-	wanted := [][]string{{"abc", "123", "456"}}
-	if err := testCSV("\"abc\",\"123\",\"456\"", wanted); err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestReadQuotedFieldMultipleLines(t *testing.T) {
-	wanted := [][]string{{"abc", "123"}, {"def", "456"}}
-	if err := testCSV("\"abc\",\"123\"\n\"def\",\"456\"", wanted); err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestReadQuotedFieldsWithComma(t *testing.T) {
-	wanted := [][]string{{"a,b,c", "d,e,f"}}
-	if err := testCSV("\"a,b,c\",\"d,e,f\"", wanted); err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestReadQuotedFieldsWithNewLine(t *testing.T) {
-	if err := testCSV("\"a\nb\nc\"", [][]string{{"a\nb\nc"}}); err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestReadQuotedFieldsWithEscapedQuotes(t *testing.T) {
-	if err := testCSV("\"a\"\"b\"", [][]string{{"a\"b"}}); err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestReadTrailingNewline(t *testing.T) {
-	if err := testCSV("a,b,c\n", [][]string{{"a", "b", "c"}}); err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestReadEmptyMiddleLine(t *testing.T) {
-	wanted := [][]string{{"a", "b"}, {""}, {"c", "d"}}
-	if err := testCSV("a,b\n\nc,d", wanted); err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestReadCRLF(t *testing.T) {
-	wanted := [][]string{{"a", "b", "c"}, {"d", "e", "f"}}
-	if err := testCSV("a,b,c\r\nd,e,f", wanted); err != nil {
-		t.Fatal(err)
-	}
-}
-
-func BenchmarkStdCsv(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		r := csv.NewReader(bytes.NewReader(data))
-		for {
-			if _, err := r.Read(); err != nil {
-				if err == io.EOF {
-					break
+	for _, testCase := range testCases {
+		t.Run(testCase.Title, func(t *testing.T) {
+			r := NewReader(strings.NewReader(testCase.Input))
+			for i, wantedLine := range testCase.Wanted {
+				fields, err := r.Read()
+				if err != nil {
+					t.Fatalf("Unexpected error on line %d: %v", i+1, err)
 				}
-				b.Fatal(err)
+				if err := compareLine(fields, wantedLine...); err != nil {
+					t.Fatalf("Mismatch on line %d: %v", i+1, err)
+				}
+			}
+			if _, err := r.Read(); err != io.EOF {
+				t.Fatal("Wanted io.EOF; got:", err)
+			}
+		})
+	}
+}
+
+func BenchmarkRead(b *testing.B) {
+	data, err := ioutil.ReadFile("testdata/fl_insurance.csv")
+	if err != nil {
+		b.Fatal(err)
+	}
+	quotedData, err := ioutil.ReadFile("testdata/fl_insurance_quoted.csv")
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.Run("StdCsv", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			r := csv.NewReader(bytes.NewReader(data))
+			for {
+				if _, err := r.Read(); err != nil {
+					if err == io.EOF {
+						break
+					}
+					b.Fatal(err)
+				}
 			}
 		}
-	}
-}
-
-func BenchmarkFastCsv(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		r := NewReader(bytes.NewReader(data))
-		for {
-			if _, err := r.Read(); err != nil {
-				if err == io.EOF {
-					break
+	})
+	b.Run("FastCsv", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			r := NewReader(bytes.NewReader(data))
+			for {
+				if _, err := r.Read(); err != nil {
+					if err == io.EOF {
+						break
+					}
+					b.Fatal(err)
 				}
-				b.Fatal(err)
 			}
 		}
-	}
-}
-
-func BenchmarkStdCsvQuoted(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		r := csv.NewReader(bytes.NewReader(quotedData))
-		for {
-			if _, err := r.Read(); err != nil {
-				if err == io.EOF {
-					break
+	})
+	b.Run("StdCsvQuoted", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			r := csv.NewReader(bytes.NewReader(quotedData))
+			for {
+				if _, err := r.Read(); err != nil {
+					if err == io.EOF {
+						break
+					}
+					b.Fatal(err)
 				}
-				b.Fatal(err)
 			}
 		}
-	}
-}
-
-func BenchmarkFastCsvQuoted(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		r := NewReader(bytes.NewReader(quotedData))
-		for {
-			if _, err := r.Read(); err != nil {
-				if err == io.EOF {
-					break
+	})
+	b.Run("FastCsvQuoted", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			r := NewReader(bytes.NewReader(quotedData))
+			for {
+				if _, err := r.Read(); err != nil {
+					if err == io.EOF {
+						break
+					}
+					b.Fatal(err)
 				}
-				b.Fatal(err)
 			}
 		}
-	}
+	})
 }
